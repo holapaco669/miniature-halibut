9f1f48d831699cf45ac95fd49081dd53afb3a3e2
Carlos Fabian Cruz Flores <carlos.cruz86@uabc.edu.mx>
2026-05-18 02:23:02 +0000

    CloudinaryField en modelo

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,13 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Pelicula(models.Model):
-    titulo = models.CharField(max_length=200)
-    anio = models.IntegerField()
-    genero = models.CharField(max_length=100)
+    titulo      = models.CharField(max_length=200)
+    anio        = models.IntegerField()
+    genero      = models.CharField(max_length=100)
     descripcion = models.TextField()
-    duracion = models.CharField(max_length=50)
-
-    imagen = models.ImageField(upload_to='peliculas/', blank=True, null=True)
+    duracion    = models.CharField(max_length=50)
+    imagen      = CloudinaryField('image', blank=True, null=True)
 
     def __str__(self):
         return self.titulo
